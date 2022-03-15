7aa74150f4d36e9fe879f32b2b04ae0775d15093
mutabay <tyyp-mstf@hotmail.com>
2022-03-15 23:48:43 +0300

    dependencies

---
+++ client/build/precache-manifest.eff7be94e0ceae00055006e6f10b4710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5712beec9b05e4b52681b060e4b6caa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af15a97f994781cacd66",
+    "url": "/static/css/main.25ef0b3a.chunk.css"
+  },
+  {
+    "revision": "6f2f6cb6494c1c045f85",
+    "url": "/static/js/2.70b0d4ac.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.70b0d4ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af15a97f994781cacd66",
+    "url": "/static/js/main.8206c9d5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
